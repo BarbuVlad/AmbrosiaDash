efdb1168ce2072e48e0bca6f261871778f8ea0c3
BogdanCaluseriu <bogdan.caluseriu99@gmail.com>
2020-07-22 17:44:33 +0300

    Adugare search la maps

--- src/Components/GMaps.js
+++ src/Components/GMaps.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React,{ useRef } from 'react'
+import {GoogleMap,} from '@react-google-maps/api';
+import "@reach/combobox/styles.css";
+import "./GMaps.css"
+import "@reach/combobox/styles.css";
+import Search from './Search'
+import Locate from "./Locate"
+
+
 
 const containerStyle = {
     width: 'relative',
     height: '100%',
-    position:'absolute',
-    top:'0',
-    right:'0',
+    position: 'absolute',
+    top: '0',
+    right: '0',
     left: '160px'
 
 };
@@ -17,20 +24,40 @@ const center = {
 };
 
 function MyComponent() {
+
+
+    const mapRef = useRef();
+
+    const onMapLoad = React.useCallback((map) => {
+        mapRef.current = map
+    }, []);
+
+    const panTo = React.useCallback(({lat, lng}) => {
+        mapRef.current.panTo({lat, lng});
+        mapRef.current.setZoom(14);
+
+    }, []);
+
+
     return (
-        <LoadScript
-            googleMapsApiKey="AIzaSyCiqc443J4Axm0tZ_ECjOMOgRrwu56zTVA"
+
+
+        <GoogleMap
+            id="map"
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onMapLoad}
         >
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={10}
-            >
-                { /* Child components, such as markers, info windows, etc. */ }
-                <></>
-            </GoogleMap>
-        </LoadScript>
-    )
+            <Locate panTo={panTo}/>
+            <Search panTo={panTo}/>
+            { /* Child components, such as markers, info windows, etc. */}
+            <></>
+        </GoogleMap>
+
+    );
+
 }
 
 export default React.memo(MyComponent)
+
