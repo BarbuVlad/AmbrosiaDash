6f861c2279ca637025a937f0ad4e5f8f66ac1630
Mihalicu <mihalicinfo@gmail.com>
2020-07-30 14:00:23 +0300

    am rezolvat cu hover

--- src/Components/GMaps.js
+++ src/Components/GMaps.js
@@ -6,6 +6,7 @@ import "@reach/combobox/styles.css";
 import "./GMaps.css"
 import axios from 'axios'
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { Polygon } from '@react-google-maps/api'
 
 
 let redPostUrl = 'http://92.87.91.16/backend_code/api/red_marker/create.php';
@@ -23,6 +24,10 @@ let greyDeleteUrl = 'http://92.87.91.16/backend_code/api/grey_marker/delete.php'
 let yellowPostUrl='http://92.87.91.16/backend_code/api/yellow_marker/create.php';
 let yellowGetUrl='http://92.87.91.16/backend_code/api/yellow_marker/read.php';
 let yellowDeleteUrl='http://92.87.91.16/backend_code/api/yellow_marker/delete.php';
+
+let zonePostUrl='';
+let zoneGetUrl='';
+let zoneDeleteUrl='';
 
 const containerStyle = {
     width: 'relative',
@@ -109,12 +114,17 @@ function getMarkersFromServer(url,markerID,setMarkers)
 
 function MyComponent() {
 
-        function countDecimals(number)
-        {
-            var char_array = number.toString().split(""); // split every single char
-            var not_decimal = char_array.lastIndexOf(".");
-            return (not_decimal<0)?0:char_array.length - not_decimal;
-        }
+
+
+
+
+
+    function countDecimals(number)
+    {
+        var char_array = number.toString().split(""); // split every single char
+        var not_decimal = char_array.lastIndexOf(".");
+        return (not_decimal<0)?0:char_array.length - not_decimal;
+    }
     let convToTen =(num)=>  //conversie la 10 zecimale
     {
         return num.toFixed(10)
@@ -124,98 +134,98 @@ function MyComponent() {
     function deleteMarkers(lat,lng,url,color) {
 
         //remove locally from markers
-/*
-        lat = convToTen(lat)
-        lng = convToTen(lng)*/
+        /*
+                lat = convToTen(lat)
+                lng = convToTen(lng)*/
         let j;
         let tempMarkers=[]
         //------------------------------------------------red
         if(color=="red")
-        for(let i = 0; i < redMarkers.length;i++)
-        {
-
-            /*     console.log("lat: "+redMarkers[i].lat + "  "+ lat)
-                 console.log("lng: "+ redMarkers[i].lng + "   "+lng)*/
-            if(convToTen(parseFloat(redMarkers[i].lat)) === lat && convToTen(parseFloat(redMarkers[i].lng))=== lng)
+            for(let i = 0; i < redMarkers.length;i++)
             {
-                j=i;
-                for(let i=0;i<j;i++)
-                    tempMarkers.push(redMarkers[i]);
-                for(let i=j+1;i< redMarkers.length;i++)
-                    tempMarkers.push(redMarkers[i]);
 
-                setRedMarkers(tempMarkers)
-                console.log(" red ma"+ redMarkers)
+                /*     console.log("lat: "+redMarkers[i].lat + "  "+ lat)
+                     console.log("lng: "+ redMarkers[i].lng + "   "+lng)*/
+                if(convToTen(parseFloat(redMarkers[i].lat)) === lat && convToTen(parseFloat(redMarkers[i].lng))=== lng)
+                {
+                    j=i;
+                    for(let i=0;i<j;i++)
+                        tempMarkers.push(redMarkers[i]);
+                    for(let i=j+1;i< redMarkers.length;i++)
+                        tempMarkers.push(redMarkers[i]);
+
+                    setRedMarkers(tempMarkers)
+                    console.log(" red ma"+ redMarkers)
 
 
+                }
             }
-        }
         tempMarkers=[];
         //------------------------------------------blue
         if(color=="blue")
-        for(let i = 0; i < blueMarkers.length;i++)
-        {
-
-            /*     console.log("lat: "+blueMarkers[i].lat + "  "+ lat)
-                 console.log("lng: "+ blueMarkers[i].lng + "   "+lng)*/
-            if(convToTen(parseFloat(blueMarkers[i].lat)) === lat && convToTen(parseFloat(blueMarkers[i].lng)) === lng)
+            for(let i = 0; i < blueMarkers.length;i++)
             {
-                j=i;
-                for(let i=0;i<j;i++)
-                    tempMarkers.push(blueMarkers[i]);
-                for(let i=j+1;i< blueMarkers.length;i++)
-                    tempMarkers.push(blueMarkers[i]);
 
-                setBlueMarkers(tempMarkers)
-                console.log(" blue ma"+ blueMarkers)
+                /*     console.log("lat: "+blueMarkers[i].lat + "  "+ lat)
+                     console.log("lng: "+ blueMarkers[i].lng + "   "+lng)*/
+                if(convToTen(parseFloat(blueMarkers[i].lat)) === lat && convToTen(parseFloat(blueMarkers[i].lng)) === lng)
+                {
+                    j=i;
+                    for(let i=0;i<j;i++)
+                        tempMarkers.push(blueMarkers[i]);
+                    for(let i=j+1;i< blueMarkers.length;i++)
+                        tempMarkers.push(blueMarkers[i]);
+
+                    setBlueMarkers(tempMarkers)
+                    console.log(" blue ma"+ blueMarkers)
 
 
+                }
             }
-        }
         tempMarkers=[];
         //------------------------------------------grey
         if(color=="grey")
-        for(let i = 0; i < greyMarkers.length;i++)
-        {
-
-            /*     console.log("lat: "+greyMarkers[i].lat + "  "+ lat)
-                 console.log("lng: "+ greyMarkers[i].lng + "   "+lng)*/
-            if(convToTen(parseFloat(greyMarkers[i].lat)) === lat && convToTen(parseFloat(greyMarkers[i].lng)) === lng)
+            for(let i = 0; i < greyMarkers.length;i++)
             {
-                j=i;
-                for(let i=0;i<j;i++)
-                    tempMarkers.push(greyMarkers[i]);
-                for(let i=j+1;i< greyMarkers.length;i++)
-                    tempMarkers.push(greyMarkers[i]);
 
-                setGreyMarkers(tempMarkers)
-                console.log(" grey ma"+ greyMarkers)
+                /*     console.log("lat: "+greyMarkers[i].lat + "  "+ lat)
+                     console.log("lng: "+ greyMarkers[i].lng + "   "+lng)*/
+                if(convToTen(parseFloat(greyMarkers[i].lat)) === lat && convToTen(parseFloat(greyMarkers[i].lng)) === lng)
+                {
+                    j=i;
+                    for(let i=0;i<j;i++)
+                        tempMarkers.push(greyMarkers[i]);
+                    for(let i=j+1;i< greyMarkers.length;i++)
+                        tempMarkers.push(greyMarkers[i]);
+
+                    setGreyMarkers(tempMarkers)
+                    console.log(" grey ma"+ greyMarkers)
 
 
+                }
             }
-        }
         tempMarkers=[];
         //------------------------------------------yellow
         if(color=="yellow")
-        for(let i = 0; i < yellowMarkers.length;i++)
-        {
-
-            /*     console.log("lat: "+yellowMarkers[i].lat + "  "+ lat)
-                 console.log("lng: "+ yellowMarkers[i].lng + "   "+lng)*/
-            if(convToTen(parseFloat(yellowMarkers[i].lat)) === lat && convToTen(parseFloat(yellowMarkers[i].lng)) === lng)
+            for(let i = 0; i < yellowMarkers.length;i++)
             {
-                j=i;
-                for(let i=0;i<j;i++)
-                    tempMarkers.push(yellowMarkers[i]);
-                for(let i=j+1;i< yellowMarkers.length;i++)
-                    tempMarkers.push(yellowMarkers[i]);
 
-                setYellowMarkers(tempMarkers)
-                console.log(" yellow ma"+ yellowMarkers)
+                /*     console.log("lat: "+yellowMarkers[i].lat + "  "+ lat)
+                     console.log("lng: "+ yellowMarkers[i].lng + "   "+lng)*/
+                if(convToTen(parseFloat(yellowMarkers[i].lat)) === lat && convToTen(parseFloat(yellowMarkers[i].lng)) === lng)
+                {
+                    j=i;
+                    for(let i=0;i<j;i++)
+                        tempMarkers.push(yellowMarkers[i]);
+                    for(let i=j+1;i< yellowMarkers.length;i++)
+                        tempMarkers.push(yellowMarkers[i]);
+
+                    setYellowMarkers(tempMarkers)
+                    console.log(" yellow ma"+ yellowMarkers)
 
 
+                }
             }
-        }
 
 
         //stergere din markers(DB)
@@ -248,6 +258,11 @@ function MyComponent() {
     const [blueSelected, setBlueSelected] = useState(null);
     const [greySelected, setGreySelected] = useState(null);
     const [yellowSelected, setYellowSelected] = useState(null);
+
+    const [zone,setZone]=useState([]);
+    const [zoneSelected,setZoneSelected]=useState(null);
+    let zoneID=0;
+
     let markerRedID=0;
     let markerBlueID=0;
     let markerGreyID=0;
@@ -260,10 +275,12 @@ function MyComponent() {
         setBlueMarkers([])
         setGreyMarkers([])
         setYellowMarkers([])
+        //setZone([])
         getMarkersFromServer(redGetUrl,markerRedID,setRedMarkers);
         getMarkersFromServer(blueGetUrl,markerBlueID,setBlueMarkers);
         getMarkersFromServer(greyGetUrl,markerGreyID,setGreyMarkers);
         getMarkersFromServer(yellowGetUrl,markerYellowID,setYellowMarkers);
+        //getMarkersFromServer(zoneGetUrl,zoneID,setZone);
         setTimeout(alwaysGetMarkers, 60 * 1000)
     }
 
@@ -273,26 +290,31 @@ function MyComponent() {
         getMarkersFromServer(blueGetUrl,markerBlueID,setBlueMarkers);
         getMarkersFromServer(greyGetUrl,markerGreyID,setGreyMarkers);
         getMarkersFromServer(yellowGetUrl,markerYellowID,setYellowMarkers);
+        //getMarkersFromServer(zoneGetUrl,zoneID,setZone);
     },[]);
 
 
 
     const onMapClick = React.useCallback((e)=>{
-        markerRedID++;
-        setRedMarkers((current)=> [
+        markerYellowID++;
+        setYellowMarkers((current)=> [
             ... current,
             {
 
                 lat : e.latLng.lat(),
                 lng : e.latLng.lng(),
-                id : markerRedID,
+                id : markerYellowID,
 
             },
         ]);
 //apelare addserver
-        postToServer(e.latLng.lat(),e.latLng.lng(),redPostUrl)
+        postToServer(e.latLng.lat(),e.latLng.lng(),yellowPostUrl)
     },[]);
-
+    /*const onZoneLoad = polygon => {
+        console.log("polygon: ", polygon);
+    }*/
+    /*  const polygonRef = React.useRef(null);
+      const listenersRef = React.useRef([]);*/
     const mapRef = React.useRef();
     const onMapLoad = React.useCallback((map) => {
         mapRef.current = map;
@@ -302,7 +324,35 @@ function MyComponent() {
         mapRef.current.setZoom(14);
 
     }, []);
-
+    /* asta tinea de polygon
+    const onEdit = React.useCallback(() => {
+         if (polygonRef.current) {
+             const nextPath = polygonRef.current
+                 .getPath()
+                 .getArray()
+                 .map(latLng => {
+                     return { lat: latLng.lat(), lng: latLng.lng() };
+                 });
+             setPath(nextPath);
+         }
+     }, [setPath]);
+     const onLoad = React.useCallback(
+         polygon => {
+             polygonRef.current = polygon;
+             const path = polygon.getPath();
+             listenersRef.current.push(
+                 path.addListener("set_at", onEdit),
+                 path.addListener("insert_at", onEdit),
+                 path.addListener("remove_at", onEdit)
+             );
+         },
+         [onEdit]
+     );
+     const onUnmount = React.useCallback(() => {
+         listenersRef.current.forEach(lis => lis.remove());
+         polygonRef.current = null;
+     }, []);
+ */
     return (
 
 
@@ -322,6 +372,7 @@ function MyComponent() {
                 zoom={10}
                 onClick={onMapClick}
                 onLoad={onMapLoad}
+
             >
 
                 <Locate panTo={panTo}/>
@@ -363,7 +414,7 @@ function MyComponent() {
                                     onClick={()=>{
                                         console.log("deleted");
 
-                                            deleteMarkers(convToTen(parseFloat(redSelected.lat)), convToTen(parseFloat(redSelected.lng)), redDeleteUrl, "red");
+                                        deleteMarkers(convToTen(parseFloat(redSelected.lat)), convToTen(parseFloat(redSelected.lng)), redDeleteUrl, "red");
 
 
 
@@ -394,14 +445,14 @@ function MyComponent() {
 
                                         deleteMarkers(convToTen(parseFloat(redSelected.lat)),convToTen(parseFloat(redSelected.lng)),redDeleteUrl,"red");
 
-                                       /* setRedMarkers([])
-                                        setTimeout(200)
-                                        getMarkersFromServer(redGetUrl,markerRedID,setRedMarkers);*/
+                                        /* setRedMarkers([])
+                                         setTimeout(200)
+                                         getMarkersFromServer(redGetUrl,markerRedID,setRedMarkers);*/
                                         setRedSelected(null);
 
 
                                     }}
-                                    >Eradicated</button>
+                            >Eradicated</button>
 
                         </div>
 
@@ -410,26 +461,26 @@ function MyComponent() {
                     </InfoWindow>
                 ) : null}
                 <></>
-//-------------------------------------------------------------------------------------------BLUE MARKER
+                //-------------------------------------------------------------------------------------------BLUE MARKER
                 {blueMarkers.map((markerBlue)=>(
-                markerBlueID++,
+                    markerBlueID++,
 
 
-                <Marker
-                    key={markerBlueID}
-                    position={{ lat: parseFloat(markerBlue.lat), lng: parseFloat(markerBlue.lng) }}
-                    icon = {{
-                        url : "https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_blue.png",
-                    scaledSize: new window.google.maps.Size(25,43)}}
-                    onClick={() => {
-                        setBlueSelected(markerBlue);
-                        console.log(markerRedID)
-                        console.log(markerBlue.lat);
+                        <Marker
+                            key={markerBlueID}
+                            position={{ lat: parseFloat(markerBlue.lat), lng: parseFloat(markerBlue.lng) }}
+                            icon = {{
+                                url : "https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_blue.png",
+                                scaledSize: new window.google.maps.Size(25,43)}}
+                            onClick={() => {
+                                setBlueSelected(markerBlue);
+                                console.log(markerRedID)
+                                console.log(markerBlue.lat);
 
-                    }}
+                            }}
 
 
-                />
+                        />
 
 
 
@@ -465,7 +516,7 @@ function MyComponent() {
 
                     </InfoWindow>
                 ) : null}
-//----------------------------------------------------------------------------grey MARKER
+                //----------------------------------------------------------------------------grey MARKER
                 {greyMarkers.map((markerGrey)=>(
                     markerGreyID++,
 
@@ -589,11 +640,11 @@ function MyComponent() {
 
                                         postToServer(yellowSelected.lat,yellowSelected.lng,redPostUrl);
 
-                                            deleteMarkers(convToTen(parseFloat(yellowSelected.lat)), convToTen(parseFloat(yellowSelected.lng)), yellowDeleteUrl, "yellow");
+                                        deleteMarkers(convToTen(parseFloat(yellowSelected.lat)), convToTen(parseFloat(yellowSelected.lng)), yellowDeleteUrl, "yellow");
 
-                                       /* setYellowMarkers([])
-                                        setTimeout(200)
-                                        getMarkersFromServer(yellowGetUrl,markerYellowID,setYellowMarkers);*/
+                                        /* setYellowMarkers([])
+                                         setTimeout(200)
+                                         getMarkersFromServer(yellowGetUrl,markerYellowID,setYellowMarkers);*/
                                         setYellowSelected(null);
 
 
@@ -605,6 +656,81 @@ function MyComponent() {
 
                     </InfoWindow>
                 ) : null}
+                }
+                {//polygon
+                    /* {path.map((zone)=>(
+                 <Polygon
+
+                     editable
+                     draggable
+                     path={{lat1:parseFloat(zone.lat1),lng1:parseFloat(zone.lng1),lat2:parseFloat(zone.lat2),lng2:parseFloat(zone.lng2),lat3:parseFloat(zone.lat3),lng3:parseFloat(zone.lng3)}}
+                     // Event used when manipulating and adding points
+                     onMouseUp={onEdit}
+                     // Event used when dragging the whole Polygon
+                     onDragEnd={onEdit}
+                     onLoad={onLoad}
+                     onUnmount={onUnmount}
+                 />
+
+
+
+                 ))}*/}
+                {zone.map((zone)=>(
+                    zoneID++,
+
+
+                        <Marker
+                            key={zoneID}
+                            position={{ lat: parseFloat(zone.lat), lng: parseFloat(zone.lng) }}
+                            icon = {{
+                                url : "https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_green.png",
+
+                                scaledSize: new window.google.maps.Size(30,60)}}
+                            style={{opacity: 0.9}}
+                            onClick={() => {
+                                setZoneSelected(zone);
+                                console.log(markerRedID)
+                                console.log(zone.lat);
+
+                            }}
+
+
+                        />
+
+
+
+                ))}
+                {zoneSelected ? (
+
+                    <InfoWindow
+
+                        position={{ lat: zoneSelected.lat+0.0003, lng: zoneSelected.lng }}
+
+                        onCloseClick={() => {
+                            setZoneSelected(null);
+
+                        }}
+                    >
+                        <div>
+                            <h2> There IS Ambrosia!</h2>
+                            <button className={"removeGrey-marker"}
+                                    onClick={()=>{
+                                        console.log("deleted");
+                                        //deleteMarkers(convToTen(parseFloat(zoneSelected.lat)),convToTen(parseFloat(zoneSelected.lng)),zoneDeleteUrl,"zone");
+
+
+                                        setZoneSelected(null);
+
+                                    }}
+                            >
+                                Remove</button>
+                        </div>
+
+
+
+                    </InfoWindow>
+                ) : null}
+
             </GoogleMap>
         </div>
 
